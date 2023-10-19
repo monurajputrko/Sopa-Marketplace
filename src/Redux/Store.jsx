71621c75075cb4fc21fc12911b51d15f460a9251
monurajputrko <monurko99@gmail.com>
2023-10-19 21:01:54 +0530

    Redux Updated

---
+++ src/Redux/Store.jsx
@@ -0,0 +1,8 @@
+import { createStore,applyMiddleware } from 'redux';
+import rootReducer from './Reducers';   
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer,applyMiddleware(thunk));
+
+export default store;
+
