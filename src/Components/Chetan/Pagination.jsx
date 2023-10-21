5f1698236e0e5835bc0b718b797d6fc8ddf2bbf8
Chetn11 <cnpatil00@gmail.com>
2023-10-21 16:27:46 +0530

    fetching part done

---
+++ src/Components/Chetan/Pagination.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pagination() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Pagination
--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -1,7 +1,11 @@
-import { createStore,applyMiddleware } from 'redux';
-import rootReducer from './Reducers';   
+import { legacy_createStore,applyMiddleware, combineReducers } from 'redux';
+
 import thunk from 'redux-thunk';
+import {ProductsReducer} from "./ProductReducer/ProductReducer.js"
 
-const store = createStore(rootReducer,applyMiddleware(thunk));
 
-export default store;
+const rootReducer=combineReducers({ProductsReducer});
+
+export  const store = legacy_createStore(rootReducer,applyMiddleware(thunk));
+
+
